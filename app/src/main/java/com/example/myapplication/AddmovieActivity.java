f724b8e1b84d19b4bdc18d4caec82b6387897fec
Rivindu2 <rivigith@gmail.com>
2021-09-22 20:24:52 +0530

    RIVINDU COMPLETE CRUD

--- app/src/main/java/com/example/myapplication/AddmovieActivity.java
+++ app/src/main/java/com/example/myapplication/AddmovieActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,9 +22,10 @@ public class AddmovieActivity extends AppCompatActivity {
 
 
 
-    private EditText movieName,movieCat,movieDuration,movieImg;
+    private EditText movieName,movieCat,movieDuration;
     private Button addMovieBtn,showMoviesBtn;
     private FirebaseFirestore db;
+    private String aId,aName,aCat,aDuration;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,40 +34,94 @@ public class AddmovieActivity extends AppCompatActivity {
         movieName=findViewById(R.id.idMovieNameEdt);
         movieCat=findViewById(R.id.idMovieCatEdt);
         movieDuration=findViewById(R.id.idMovieDurationtEdt);
-        movieImg=findViewById(R.id.idMovieImagetEdt);
         addMovieBtn=findViewById(R.id.idtAddmovieBtn);
         showMoviesBtn=findViewById(R.id.idtShowMoviesBtn);
 
         db=FirebaseFirestore.getInstance();
 
-        addMovieBtn.setOnClickListener(new View.OnClickListener() {
+        Bundle bundle=getIntent().getExtras();
+        if(bundle!=null){
+            addMovieBtn.setText("Update");
+
+            aId=bundle.getString("aId");
+            aName=bundle.getString("aName");
+            aCat=bundle.getString("aCat");
+            aDuration=bundle.getString("aDuration");
+
+            movieName.setText(aName);//assign to new feild
+            movieCat.setText(aCat);
+            movieDuration.setText(aDuration);
+
+
+        }else {
+            addMovieBtn.setText("Add Movie");
+        }
+
+        showMoviesBtn.setOnClickListener(new View.OnClickListener() {//onclick view movies
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AddmovieActivity.this,ViewMovieActivity.class));
+            }
+        });
+
+        addMovieBtn.setOnClickListener(new View.OnClickListener() {//Onclick addd movie button
             @Override
             public void onClick(View view) {
                 String name=movieName.getText().toString();
                 String cat=movieCat.getText().toString();
-                String duration=movieDuration.getText().toString();
-                String img=movieImg.getText().toString();
-                String id= UUID.randomUUID().toString();
+                float division=Integer.parseInt(movieDuration.getText().toString());
+                float s=division/60;
+                String duration=String.valueOf(s);
 
-                saveToFireStore(id,name,cat,duration,img);
+                 Bundle bundle1=getIntent().getExtras();//check if user wants to update data or add new data
+                 if (bundle1!=null){
+                     String id=aId;
+                     updatetoFirestore(id,name,cat,duration);
 
+                 }else {
+                     String id= UUID.randomUUID().toString();//generat random variable
+                     saveToFireStore(id,name,cat,duration);
+
+                 }
             }
         });
     }
-    private void saveToFireStore(String id,String name,String cat,String duration,String img){
-        if(!name.isEmpty() && !cat.isEmpty() && !duration.isEmpty() && !img.isEmpty()){
+    private void updatetoFirestore(String id,String name,String cat,String duration){
+        db.collection("MovieDocuments").document(id).update("name",name,"cat",cat,"duration",duration).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(AddmovieActivity.this, "Data Successfully updated", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(AddmovieActivity.this, "Error"+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(AddmovieActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private void saveToFireStore(String id,String name,String cat,String duration){///to save to firestore
+        if(!name.isEmpty() && !cat.isEmpty() && !duration.isEmpty()){
             HashMap<String,Object> map=new HashMap<>();
             map.put("id", id);
             map.put("name", name);
             map.put("cat", cat);
             map.put("duration", duration);
-            map.put("img", img);
 
             db.collection("MovieDocuments").document(id).set(map).addOnCompleteListener(new OnCompleteListener<Void>() {
                 @Override
                 public void onComplete(@NonNull Task<Void> task) {
                     if(task.isSuccessful()){
                         Toast.makeText(AddmovieActivity.this, "Movie Added", Toast.LENGTH_SHORT).show();
+                        //Intent intent=new Intent(AddmovieActivity.this,ViewMovieActivity.class);
+                        //startActivity(intent);
+                        Reset();
+
                     }
                 }
             }).addOnFailureListener(new OnFailureListener() {
@@ -78,5 +134,11 @@ public class AddmovieActivity extends AppCompatActivity {
 
         }else
             Toast.makeText(AddmovieActivity.this, "Fields empty", Toast.LENGTH_SHORT).show();
+    }
+    private void Reset(){//reset form
+        movieName.setText("");
+        movieCat.setText("");
+        movieDuration.setText("");
+
     }
 }
