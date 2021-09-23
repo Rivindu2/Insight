0561a5868e367f6324952034dfb24b9eb0b124b8
Chama1927 <it20038700@my.sliit.lk>
2021-09-23 23:02:31 +0530

    CardUPDATEDELETE

--- app/src/main/java/com/example/myapplication/ViewCardActivity.java
+++ app/src/main/java/com/example/myapplication/ViewCardActivity.java
@@ -1,14 +1,74 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ViewCardActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private FirebaseFirestore db;
+    private MyCardAdapter adapter;
+    private List<CardModel> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_card);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        db = FirebaseFirestore.getInstance();
+        list = new ArrayList<>();
+        adapter = new MyCardAdapter(this , list);
+        recyclerView.setAdapter(adapter);
+
+        ItemTouchHelper touchHelper = new ItemTouchHelper(new CardTouchHelper(adapter));
+        touchHelper.attachToRecyclerView(recyclerView);
+
+        showData();
+    }
+
+    public void showData(){
+
+        db.collection("Card_Details").get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @SuppressLint("NotifyDataSetChanged")
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        list.clear();
+                        for (DocumentSnapshot snapshot : task.getResult()){
+
+                            CardModel model = new CardModel(snapshot.getString("id") , snapshot.getString("Input1") , snapshot.getString("Input2") , snapshot.getString("Input3") , snapshot.getString("Input4"));
+                            list.add(model);
+                        }
+                        adapter.notifyDataSetChanged();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(ViewCardActivity.this, "Oops.. Something went wrong", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
