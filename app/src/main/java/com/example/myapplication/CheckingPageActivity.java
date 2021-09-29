21665722dc2b90dd7f2bb608ee298fb69761c36f
Chama1927 <it20038700@my.sliit.lk>
2021-09-29 08:16:08 +0530

    alertboxmodifypart

--- app/src/main/java/com/example/myapplication/CheckingPageActivity.java
+++ app/src/main/java/com/example/myapplication/CheckingPageActivity.java
@@ -2,11 +2,16 @@ package com.example.myapplication;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,11 +34,19 @@ import java.util.UUID;
 public class CheckingPageActivity extends AppCompatActivity {
     TextView Mid , Mname , Noofseats , Showdate , Tprice;
     TextView fullName,email,phone;
+
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
     String userId;
     String bid;
     FirebaseUser user;
+    private FirebaseFirestore db;
+    private List<CardModel> list;
+    Button btn_success;
+
+    //dialog variables
+    AlertDialog.Builder builderDialog;
+    AlertDialog alertDialog;
 
     DocumentReference documentReference;
 
@@ -46,6 +59,17 @@ public class CheckingPageActivity extends AppCompatActivity {
         Noofseats = findViewById(R.id.Noofseats);
         Showdate = findViewById(R.id.Showdate);
         Tprice = findViewById(R.id.Tprice);
+        btn_success = (Button) findViewById(R.id.btn_paynow);
+
+        btn_success.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showAlertDialog(R.layout.my_success_dialog);
+
+            }
+        });
+
+
 
         fAuth = FirebaseAuth.getInstance();
         fStore = FirebaseFirestore.getInstance();
@@ -56,21 +80,46 @@ public class CheckingPageActivity extends AppCompatActivity {
 
 
 
-       documentReference = fStore.collection("users").document(userId);
+
+
+
+
+
+       documentReference = fStore.collection("users").document(userId).collection("BookingInfo").document("f3a16db8-056e-4e62-af1d-42816fe5261f");
         documentReference.addSnapshotListener(CheckingPageActivity.this, new EventListener<DocumentSnapshot>() {
             @Override
             public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
                 if(documentSnapshot.exists()){
-                    Mid.setText(documentSnapshot.getString("Mid"));
-                    Mname.setText(documentSnapshot.getString("Mname"));
-                    Noofseats.setText(documentSnapshot.getString("Noofseats"));
-                    Showdate.setText(documentSnapshot.getString("Showdate"));
-                    Tprice.setText(documentSnapshot.getString("Tprice"));
+                    Mid.setText(documentSnapshot.getString("mid"));
+                    Mname.setText(documentSnapshot.getString("mname"));
+                    Noofseats.setText(documentSnapshot.getString("noofseats"));
+                    Showdate.setText(documentSnapshot.getString("showdate"));
+                    Tprice.setText(documentSnapshot.getString("tprice"));
 
 
                 }else {
                     Log.d("tag", "onEvent: Document do not exists");
                 }
+            }
+        });
+    }
+
+    private void showAlertDialog(int myLayout) {
+        builderDialog = new AlertDialog.Builder(this);
+        View layoutView = getLayoutInflater().inflate(myLayout, null);
+
+        AppCompatButton dialogButton = layoutView.findViewById(R.id.buttonOK);
+        builderDialog.setView(layoutView);
+        alertDialog = builderDialog.create();
+        alertDialog.show();
+
+        //click on OK Button
+        dialogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //dismiss dialog
+                alertDialog.dismiss();
+                startActivity(new Intent(CheckingPageActivity.this , dashboardActivity.class));
             }
         });
     }
--- app/src/main/java/com/example/myapplication/Cusbooking.java
+++ app/src/main/java/com/example/myapplication/Cusbooking.java
@@ -3,7 +3,7 @@ package com.example.myapplication;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -13,7 +13,7 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
+
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
@@ -30,7 +30,7 @@ public class Cusbooking extends AppCompatActivity {
     private String uMid, uMname, uNoofseats, uShowdate, uTprice, uId;
     String userId;
     FirebaseAuth fAuth;
-    FirebaseFirestore fStore;
+
 
 
     @Override
@@ -44,7 +44,7 @@ public class Cusbooking extends AppCompatActivity {
         Showdate=findViewById(R.id.textView23);
         Tprice=findViewById(R.id.textView24);
         EBtn=findViewById(R.id.btn10);
-        EVBtn=findViewById(R.id.button3);
+        EVBtn=findViewById(R.id.btn9);
 
 
         fAuth = FirebaseAuth.getInstance();
@@ -71,7 +71,7 @@ public class Cusbooking extends AppCompatActivity {
         EVBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(Cusbooking.this , ShowbookingActivity.class));
+                startActivity(new Intent(Cusbooking.this, ShowbookingActivity.class));
             }
         });
 
